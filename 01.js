45085635c2f15c00f560e83c38418d01b4ec4e49
ProgrammingGeek02 <davideveriwhere@gmail.com>
2021-09-24 21:34:15 -0500

    Adding crud with VueJS & LocalStorage/Bootstrap_4

--- 01.js
+++ 01.js
@@ -7,7 +7,37 @@ const app = new Vue({
             {nombre: 'Pera', cantidad: 10},
             {nombre: 'Manzana', cantidad: 0},
             {nombre: 'Platano', cantidad: 11},
-        ]
+        ],
+        inputFruta: {
+            nuevaFruta: '',
+            cantidadFruta: 0
+        },
+        total: 0
+    },
+    methods: {
+        agregarFruta () {
+
+            this.frutas.push({
+                nombre: this.inputFruta.nuevaFruta, cantidad: this.inputFruta.cantidadFruta
+            });
+            this.inputFruta = {
+                nuevaFruta: '',
+                cantidadFruta: ''
+            }
+
+        },
+        otroMetodo() {
+
+        }
+    },
+    computed: {
+        sumarFrutas() {
+            this.total = 0;
+            for(fruta of this.frutas) {
+                this.total = this.total + fruta.cantidad;
+            }
+            return this.total;
+        }
     }
 
 });
